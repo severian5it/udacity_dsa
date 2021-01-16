fc889c9aacd2c3146dfbf150a21d5aa00d8acd39
Pierluca Del Buono <pierluca@amazon.com>
2021-01-16 21:55:05 +0100

    rearrage digit with permutation

--- basic_algorithm/problems_vs_algorithm/3Rearrange_digits.py
+++ basic_algorithm/problems_vs_algorithm/3Rearrange_digits.py
@@ -1,3 +1,32 @@
+import copy
+
+def perm_rec(output, input):
+    if not input:
+        return output, input
+    elif output == [[]]:
+        a = input.pop()
+        return perm_rec([[a]], input)
+    else:
+        l = len(output)
+        a = input.pop()
+        new_output = []
+        for i in range(l+1):
+            for j in output:
+                new_list = copy.deepcopy(j)
+                new_list.insert(i, a)
+                new_output.append(new_list)
+        return perm_rec(new_output, input)
+
+
+def permute(inputList):
+    """
+    Args: myList: list of items to be permuted
+    Returns: list of permutation with each permuted item being represented by a list
+    """
+    out, _ = perm_rec([[]], inputList)
+    return out
+
+
 def rearrange_digits(input_list):
     """
     Rearrange Array Elements so as to form two number such that their sum is maximum.
@@ -7,7 +36,20 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    pass
+    break_idx = len(input_list)//2
+    output = permute(input_list)
+    max, max_array = 0, [0, 0]
+
+    for o in output:
+        a = int("".join(map(str, o[:break_idx])))
+        b = int("".join(map(str, o[break_idx:])))
+        c = a + b
+        if c >= max:
+            max = c
+            max_array = [b, a]
+
+    return max_array
+
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
@@ -17,5 +59,7 @@ def test_function(test_case):
     else:
         print("Fail")
 
+
 test_function([[1, 2, 3, 4, 5], [542, 31]])
-test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
+
