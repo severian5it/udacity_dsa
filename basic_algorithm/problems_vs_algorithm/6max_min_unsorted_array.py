19f2f397b8a65cd4569aba69e676203fadecda67
Pierluca Del Buono <pierluca@amazon.com>
2021-01-17 12:00:59 +0100

    documentation and test cases min and max

--- basic_algorithm/problems_vs_algorithm/6max_min_unsorted_array.py
+++ basic_algorithm/problems_vs_algorithm/6max_min_unsorted_array.py
@@ -1,4 +1,14 @@
+import random
+
+
 def get_min_max(ints):
+    '''  returning max and min in a integer list
+    args:
+      ints: an unsorted array of integers
+
+    returns:
+      (max, min): tuple containing max and min
+    '''
     if not ints:
         return (None, None)
 
@@ -13,10 +23,16 @@ def get_min_max(ints):
 
     return (min, max)
 
-## Example Test Case of Ten Integers
-import random
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
-print(l)
 print("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+l = [1,2,3]
+print("Pass" if ((1, 3) == get_min_max(l)) else "Fail")
+
+# edge cases
+
+l = []
+print(get_min_max(l)) # should return None, None
+l = None
+print(get_min_max(l)) # should return None, None
