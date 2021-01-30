b61f43f86847246b2e090f5b6b529260c81cc703
Pierluca Del Buono <pierluca@amazon.com>
2021-01-30 10:38:17 +0100

    BFS for Graph

---
+++ advanced_algorithm/Graph/BFS_graph.py
@@ -0,0 +1,91 @@
+class GraphNode(object):
+    def __init__(self, val):
+        self.value = val
+        self.children = []
+
+    def add_child(self, new_node):
+        self.children.append(new_node)
+
+    def remove_child(self, del_node):
+        if del_node in self.children:
+            self.children.remove(del_node)
+
+
+class Graph(object):
+    def __init__(self, node_list):
+        self.nodes = node_list
+
+    def add_edge(self, node1, node2):
+        if (node1 in self.nodes and node2 in self.nodes):
+            node1.add_child(node2)
+            node2.add_child(node1)
+
+    def remove_edge(self, node1, node2):
+        if (node1 in self.nodes and node2 in self.nodes):
+            node1.remove_child(node2)
+            node2.remove_child(node1)
+
+nodeG = GraphNode('G')
+nodeR = GraphNode('R')
+nodeA = GraphNode('A')
+nodeP = GraphNode('P')
+nodeH = GraphNode('H')
+nodeS = GraphNode('S')
+
+graph1 = Graph([nodeS,nodeH,nodeG,nodeP,nodeR,nodeA] )
+graph1.add_edge(nodeG,nodeR)
+graph1.add_edge(nodeA,nodeR)
+graph1.add_edge(nodeA,nodeG)
+graph1.add_edge(nodeR,nodeP)
+graph1.add_edge(nodeH,nodeG)
+graph1.add_edge(nodeH,nodeP)
+graph1.add_edge(nodeS,nodeR)
+
+for each in graph1.nodes:
+    print('parent node = ',each.value,end='\nchildren\n')
+    for each in each.children:
+        print(each.value,end=' ')
+    print('\n')
+'''
+visited = set()  # Sets are faster for lookups
+    stack = [root_node]  # Start with a given root node
+
+    while len(stack) > 0:  # Repeat until the stack is empty
+
+        current_node = stack.pop()  # Pop out a node added recently
+        visited.add(current_node)  # Mark it as visited
+
+        if current_node.value == search_value:
+            return current_node
+
+        # Check all the neighbours
+        for child in current_node.children:
+
+            # If a node hasn't been visited before, and not available in the stack already.
+            if (child not in visited) and (child not in stack):
+                stack.append(child)
+'''
+
+
+def bfs_search(root_node, search_value):
+    visited = set()  # Sets are faster for lookups
+    q = [root_node]  # Start with a given root node
+
+    while len(q) > 0:  # Repeat until the stack is empty
+        current_node = q.pop(0)  # Pop out a node added recently
+        visited.add(current_node)  # Mark it as visited
+
+        if current_node.value == search_value:
+            return current_node
+
+        # Check all the neighbours
+        for child in current_node.children:
+
+            # If a node hasn't been visited before, and not available in the stack already.
+            if (child not in visited) and (child not in q):
+                q.append(child)
+
+
+assert nodeA == bfs_search(nodeS, 'A')
+assert nodeS == bfs_search(nodeP, 'S')
+assert nodeR == bfs_search(nodeH, 'R')
