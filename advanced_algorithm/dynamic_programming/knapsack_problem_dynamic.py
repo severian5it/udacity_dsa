6c26587ca9dc7dd6475d8edeca8c4d91576cb669
Pierluca Del Buono <pierluca@amazon.com>
2021-02-02 22:24:14 +0100

    Knapsack problem recursive

---
+++ advanced_algorithm/dynamic_programming/knapsack_problem_dynamic.py
@@ -0,0 +1,26 @@
+import collections
+
+# An item can be represented as a namedtuple
+Item = collections.namedtuple('Item', ['weight', 'value'])
+
+def knapsack_max_value(knapsack_max_weight, items):
+    """
+    Get the maximum value of the knapsack.
+    """
+    pass
+
+tests = [
+    {
+        'correct_output': 14,
+        'input':
+            {
+                'knapsack_max_weight': 15,
+                'items': [Item(10, 7), Item(9, 8), Item(5, 6)]}},
+    {
+        'correct_output': 13,
+        'input':
+            {
+                'knapsack_max_weight': 25,
+                'items': [Item(10, 2), Item(29, 10), Item(5, 7), Item(5, 3), Item(5, 1), Item(24, 12)]}}]
+for test in tests:
+    assert test['correct_output'] == knapsack_max_value(**test['input'])
