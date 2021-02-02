ec4bfb7927e77bcf8ba028b81a72109f44c2856a
Pierluca Del Buono <pierluca@amazon.com>
2021-02-02 23:54:39 +0100

    Knapsack dynamic Problem

--- advanced_algorithm/dynamic_programming/knapsack_problem_dynamic.py
+++ advanced_algorithm/dynamic_programming/knapsack_problem_dynamic.py
@@ -1,4 +1,5 @@
 import collections
+from operator import attrgetter
 
 # An item can be represented as a namedtuple
 Item = collections.namedtuple('Item', ['weight', 'value'])
@@ -7,7 +8,14 @@ def knapsack_max_value(knapsack_max_weight, items):
     """
     Get the maximum value of the knapsack.
     """
-    pass
+    max_per_weight = [0]*(knapsack_max_weight+1)
+
+    for item in sorted(items, key=attrgetter('weight')):
+        for capacity in reversed(range(knapsack_max_weight + 1)):
+            if capacity >= item.weight:
+                max_per_weight[capacity] = max(max_per_weight[capacity - item.weight] + item.value, max_per_weight[capacity])
+
+    return max_per_weight[-1]
 
 tests = [
     {
